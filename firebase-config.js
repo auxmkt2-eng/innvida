ceea6ed3380e6818a403064fb2e9d5c7a6c27c38
auxmkt2-eng <aux.mkt2@innvida.es>
2026-08-27 16:57:12 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -16,5 +16,13 @@ export const firebaseProjects = {
     storageBucket: "cotizador-nomad.firebasestorage.app",
     messagingSenderId: "736481537624",
     appId: "1:736481537624:web:6f06667cf34bccc532642d"
+  },
+  nuevoSanare: {
+    apiKey: "AIzaSyCUFENXy1PE7Q6lX7c54F8hH3RjStM9Fdc",
+    authDomain: "cotizador-30.firebaseapp.com",
+    projectId: "cotizador-30",
+    storageBucket: "cotizador-30.firebasestorage.app",
+    messagingSenderId: "150005004914",
+    appId: "1:150005004914:web:5b217c06aa13e34b9960eb"
   }
 };
